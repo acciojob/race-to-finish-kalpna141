90e6ce4a6af904d3f4b4ebbcd78f778ff3d6f700
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-24 13:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,39 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let a=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("a");
+	},3000)
+})
+let b=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("b");
+	},2000)
+})
+
+let c=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("c");
+	},1000)
+})
+
+let d=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("d");
+	},4000)
+})
+
+let e=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("e");
+	},5000)
+})
+
+let x= Promise.any([a,b,c,d, e])
+{
+	x.then((data)=>{
+		const elem= document.getElementById('output');
+		elem.innerText=data;
+	})
+}
